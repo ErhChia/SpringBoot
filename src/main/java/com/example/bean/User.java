c66836c8b8260f029b20f285219eaf0733321c58
ECLin <jack40062006@gmail.com>
2018-11-28 17:44:56 +0800

    Change package name

--- src/main/java/com/example/bean/User.java
+++ src/main/java/com/example/bean/User.java
@@ -1,4 +1,4 @@
-package com.example.domain;
+package com.example.bean;
 
 public class User {
     private int userId;
--- src/test/java/com/example/bean/UserTest.java
+++ src/test/java/com/example/bean/UserTest.java
@@ -1,4 +1,4 @@
-package com.example.domain;
+package com.example.bean;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -6,7 +6,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.ArrayList;
-import java.util.List;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
