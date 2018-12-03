89aa42974bbaf8d3d2ba83413223dd7c755887a0
ECLin <jack40062006@gmail.com>
2018-12-03 13:47:21 +0800

    optimize imports

--- src/main/java/com/example/repository/StudentRepository.java
+++ src/main/java/com/example/repository/StudentRepository.java
@@ -1,13 +1,10 @@
 package com.example.repository;
 
-import com.example.bean.Clazz;
 import com.example.bean.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
-import java.util.List;
-
-public interface StudentRepository extends JpaRepository<Student,String> {
+public interface StudentRepository extends JpaRepository<Student, String> {
     Student findByNo(String no);
-    Student findByIdAndNo(String id,String no);
+
+    Student findByIdAndNo(String id, String no);
 }
